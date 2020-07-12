cc4538ac374e430192497d54282f8e2aca1cd6e5
“Shubham” <shubhampattar2@gmail.com>
2020-07-12 16:47:43 +0530

    ADDED Test Case And Code To Get Feet In Inches

--- src/test/java/QualityMeasurementTest.java
+++ src/test/java/QualityMeasurementTest.java
@@ -1,3 +1,10 @@
-public class QualityMeasurementTest {
+import com.bridgelabz.qualitymeasurement.services.QualityMeasurement;
+import org.junit.Test;
 
+public class QualityMeasurementTest {
+    @Test
+    public void givenLengthInFeet_ShouldReturnEquivalentValueInInches() {
+        Double inchesForFeet = new QualityMeasurement().getInchesForFeet(2.5);
+        System.out.println(inchesForFeet);
+    }
 }
