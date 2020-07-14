2c01583688b00512dd8c8b057751162860b821b4
“Shubham” <shubhampattar2@gmail.com>
2020-07-14 21:18:48 +0530

    REFACTORED Code For Null Check, Type Check, Value Check, Reference Check.

--- src/main/java/com/bridgelabz/quantitymeasurement/services/QuantityMeasurement.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/services/QuantityMeasurement.java
@@ -4,28 +4,21 @@ import com.bridgelabz.quantitymeasurement.exception.QuantityMeasurementException
 import com.bridgelabz.quantitymeasurement.utility.UnitType;
 
 public class QuantityMeasurement {
-
     private double value;
 
-    public QuantityMeasurement() {
-    }
+    public QuantityMeasurement(UnitType unitType, Double value) throws QuantityMeasurementException {
+        if (value == null)
+            throw new QuantityMeasurementException("Null Value Provided",
+                    QuantityMeasurementException.ExceptionType.NULL_VALUE_PROVIDED);
+        this.value = (value * unitType.value);
 
-    public double convert(UnitType unitType, Double value) throws QuantityMeasurementException {
-        if (value == null) {
-            throw new QuantityMeasurementException("NULL Value Provided", QuantityMeasurementException.ExceptionType.NULL_VALUE_PROVIDED);
-        }
-        return (value * unitType.value);
-    }
-
-    public boolean compare(double value1, double value2) {
-        return Double.compare(value1, value2) == 0;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;         //ref check
-        if (o == null || getClass() != o.getClass()) return false;      //null n type check
-        QuantityMeasurement quantityMeasurement = (QuantityMeasurement) o;  //converting obj param to type of QM
-        return this.compare(quantityMeasurement.value, value);      //value check
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        QuantityMeasurement that = (QuantityMeasurement) o;
+        return Double.compare(that.value, value) == 0;
     }
 }
