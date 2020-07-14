ab8a3118f468cf6ff2ecd93587c5b886b20782f3
“Shubham” <shubhampattar2@gmail.com>
2020-07-14 22:02:24 +0530

    ADDED More Test Cases For Centimeter Check With Feet And Yard.

--- src/main/java/com/bridgelabz/quantitymeasurement/utility/UnitType.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/utility/UnitType.java
@@ -2,7 +2,7 @@ package com.bridgelabz.quantitymeasurement.utility;
 
 public enum UnitType {
 
-    INCH(1), FEET(12), YARD(36), CENTIMETER(1/2.5);
+    INCH(1), FEET(12), YARD(36), CENTIMETER(0.4);
 
     public final double value;
 
